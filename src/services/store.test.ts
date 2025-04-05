60f4092284e78ecf576319558200df9e3dfa6993
Ekaterina <solovyovaalw@gmail.com>
2025-04-05 04:41:57 +0300

    Исправлен тест для корневого редьюсера

--- src/services/store.test.ts
+++ src/services/store.test.ts
@@ -1,57 +1,24 @@
-import { configureStore } from '@reduxjs/toolkit';
 import { rootReducer } from '../services/store';
-import {
-  initialState as burgerConstructorInit,
-  burgerConstructorSlice
-} from './slice/burgerConstructorSlice/burgerConstructorSlice';
+import { initialState as burgerConstructorInit } from './slice/burgerConstructorSlice/burgerConstructorSlice';
 import { initialState as ingredientsInit } from './slice/ingredientsSlice/ingredientsSlice';
 import { initialState as orderInit } from './slice/orderSlice/orderSlice';
 import { initialState as profileOrdersInit } from './slice/profileOrderSlice/profileOrdersSlice';
 import { initialState as feedInit } from './slice/feedSlice/feedSlice';
 import { initialState as userInit } from './slice/userSlice/userSlice';
 
-describe('should initialize with the correct default state rootReducer', () => {
-  const store = configureStore({
-    reducer: rootReducer,
-    preloadedState: {
-      'burger-constructor': burgerConstructorInit,
+describe('rootReducer', () => {
+  it('should return the initial state when passed an unknown action', () => {
+    const unknownAction = { type: 'UNKNOWN_ACTION' };
+
+    const nextState = rootReducer(undefined, unknownAction);
+
+    expect(nextState).toEqual({
       ingredients: ingredientsInit,
+      'burger-constructor': burgerConstructorInit,
       order: orderInit,
-      'profile-orders': profileOrdersInit,
       feed: feedInit,
-      user: userInit
-    }
-  });
-
-  test('burgerConstructor', () => {
-    expect(store.getState()['burger-constructor']).toEqual(
-      burgerConstructorSlice.reducer(burgerConstructorInit, {
-        type: 'UNKNOWN_ACTION'
-      })
-    );
-
-    const addIngredient = { type: 'addIngredient' };
-    store.dispatch(addIngredient);
-    expect(store.getState()['burger-constructor']).toEqual(
-      burgerConstructorInit
-    );
-
-    const removeIngredient = { type: 'removeIngredient' };
-    store.dispatch(removeIngredient);
-    expect(store.getState()['burger-constructor']).toEqual(
-      burgerConstructorInit
-    );
-
-    const moveIngredient = { type: 'moveIngredient' };
-    store.dispatch(moveIngredient);
-    expect(store.getState()['burger-constructor']).toEqual(
-      burgerConstructorInit
-    );
-
-    const clearConstructor = { type: 'clearConstructor' };
-    store.dispatch(clearConstructor);
-    expect(store.getState()['burger-constructor']).toEqual(
-      burgerConstructorInit
-    );
+      user: userInit,
+      'profile-orders': profileOrdersInit
+    });
   });
 });
